02babdd797f9828811097e893ffcb4c051dc4894
shel-luc <51696132+shel-luc@users.noreply.github.com>
2020-06-21 16:34:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 
-
-const express = require('express');
-
-const port = process.env.PORT || 3000;
-
+var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', (req, res) => {
-  var result = {};
-  result.ipaddress = req.ip.match(/\d+\.\d+\.\d+\.\d+/)[0];
-  result.language = req.acceptsLanguages()[0];
-  result.software = req.get('user-agent').match(/\((.+?)\)/)[1];
-  //console.log(req.ip);
-  res.send(result);
+    res.redirect('/api/whoami');
 });
 
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}.`);
+app.get('/api/whoami', (req, res) => {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    var language = req.headers['accept-language'].split(',')[0];
+    
+    var regexp = /\(([^()]*)\)/g;
+    var software = regexp.exec(req.headers['user-agent'])[1];
+    var data = {
+        ip: ip,
+        language: language,
+        software: software,
+    }
+    res.json(data);
+});
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
 });
